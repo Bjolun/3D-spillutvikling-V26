1d483a4087ea17f39971e691d2fcdcc1c760249e
Bjørn Arild Lunde <bjolun5@gmail.com>
2026-01-29 15:40:04 +0100

    Update CameraManager.cs

--- CameraManager.cs
+++ CameraManager.cs
@@ -4,22 +4,29 @@ using UnityEngine.InputSystem;
 
 public class CameraManager : MonoBehaviour
 {
+    // Referanse til cinemachine kameraene våre.
     [SerializeField] private CinemachineCamera staticCamera;
     [SerializeField] private CinemachineCamera orbitalCamera;
 
+    // En bool for å holde styr på om et kamera er aktivt eller ikke aktivt
     private bool isStaticCameraActive = false;
 
     // Update is called once per frame
     void Update()
     {
+        // Hvis spilleren trykker på spacebar skal vi bytte kamera
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
+        {    
+            // Setter isStaticCameraActive til motsatt av hva det er nå (true/false)
             isStaticCameraActive = !isStaticCameraActive;
-            
+
+            // Logger isStaticCameraActiv for å se så den endres som forventet (kan fjernes)
             Debug.Log(isStaticCameraActive);
 
+            // Vi sjekker om isStaticCameraActiv er true eller ikke
             if (isStaticCameraActive)
             {
+                // Endrer prioritet på kameraene slik at vi får et nytt aktivt kamera.
                 staticCamera.Priority = 1;
                 orbitalCamera.Priority = 0;
             }
@@ -31,3 +38,4 @@ public class CameraManager : MonoBehaviour
         }
     }
 }
+
