16b71eebecdcdae044730a0666d369dba0d1abdd
Bjørn Arild Lunde <bjolun5@gmail.com>
2026-02-13 11:48:22 +0100

    La til kommentarer

--- DestroyCoins.cs
+++ DestroyCoins.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class DestroyCoins : MonoBehaviour
 {
+    // Referanser til komponentene våre
     private AudioSource _audioSource;
     private MeshRenderer _meshRenderer;
 
@@ -12,11 +13,15 @@ public class DestroyCoins : MonoBehaviour
         _meshRenderer = GetComponent<MeshRenderer>();
     }
 
-    // Når vi kolliderer med noe, ødelegg game objectet.
+    // Når vi kolliderer med noe, ødelegg game objectet og spill av lyd
     private void OnTriggerEnter(Collider other)
     {
+        // Spill av lydklippet vårt
         _audioSource.Play();
+        // Disable mesh renderer komponenten så mynten blir usynlig uten at den blir ødelagt.
         _meshRenderer.enabled = false;
+        // Ødelegg dette game objektet etter at lydklippet har spilt ferdig.
         Destroy(gameObject, _audioSource.clip.length);
     }
 }
+
